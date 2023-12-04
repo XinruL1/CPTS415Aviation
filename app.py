741e5f9a158d2ddd33b42d1e0209362db0005325
XinruL1 <89551461+XinruL1@users.noreply.github.com>
2023-12-04 12:38:46 +0000

    Created Airports Search page and linked to the index page Airport Search button.

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask.templating import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/airports')
+def airports():
+    return render_template('airports.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
