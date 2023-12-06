d5bc763f014e332d90c41bd8b1dd2b72ca3b6856
Xinru Li <xinru.li1@wsu.edu>
2023-12-06 09:07:08 -0800

    Implemented Airport Search feature, it is now able to query and display result based on user inputs.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-from flask.templating import render_template
+from flask import Flask, render_template, url_for, request, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.Aviation
+airports_collection = db.Airports
 
 @app.route('/')
 def index():
@@ -9,7 +13,7 @@ def index():
 
 @app.route('/airports')
 def airports():
-    return render_template('airports.html')
+  return render_template('airports.html')
 
 @app.route('/airlines')
 def airlines():
@@ -22,6 +26,32 @@ def aggregation():
 @app.route('/recommendation')
 def recommendation():
   return render_template('recommendation.html')
+
+@app.route('/search_airports', methods=['GET','POST'])
+def search_airports():
+  query = {}
+
+  #read user inputs
+  country = request.args.get('country')
+  city = request.args.get('city')
+  iata = request.args.get('iata-code')
+  icao = request.args.get('icao-code')
+  name = request.args.get('airport-name')
+
+  #construct a query to filter based on all provided inputs
+  query = {
+    "Country": country,
+    "City": city,
+    "IATA": iata,
+    "ICAO": icao,
+    "Name": name
+  }
+  #drop keys with empty values
+  query = {key:value for key, value in query.items() if value}
+
+  #perform the query on the collection
+  airport_data = list(airports_collection.find(query))
+  return render_template('airportsinfo.html',airports = airport_data)
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
